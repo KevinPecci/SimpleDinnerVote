9cdc4650f64d5acf0dae92e10f32125256a314fd
Kevin Pecci <s1191114@monmouth.edu>
2020-03-24 15:22:28 -0400

    Reorganized methods for better readablity

--- vote.rb
+++ vote.rb
@@ -6,6 +6,13 @@ get '/' do
     erb :index
 end
 
+get '/results' do
+    @title = 'Results so far:'
+    @store = YAML::Store.new 'votes.yml'
+    @votes = @store.transaction { @store['votes']}
+    erb :results
+end
+
 post '/cast' do
     @title = 'Thanks for casting your vote!'
     @vote = params['vote']
@@ -16,13 +23,6 @@ post '/cast' do
         @store['votes'][@vote] += 1
     end
     erb :cast
-end
-
-get '/results' do
-    @title = 'Results so far:'
-    @store = YAML::Store.new 'votes.yml'
-    @votes = @store.transaction { @store['votes']}
-    erb :results
 end
 
 Choices = {
